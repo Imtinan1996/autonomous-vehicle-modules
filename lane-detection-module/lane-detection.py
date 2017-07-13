2401ff27f6992718b31aa71ebced22900ab3d6dd
Imtinan Azhar <imtinan@windowslive.com>
2017-07-13 15:13:29 +0500

    Update lane-detection.py

--- lane-detection-module/lane-detection.py
+++ lane-detection-module/lane-detection.py
@@ -3,7 +3,9 @@ import cv2
 import pyautogui
 import matplotlib.pyplot as plt
 
-area_to_remove=np.array([[100,474],[100,375],[300,200],[520,200],[800,375],[800,475]])
+#area_to_remove=np.array([[100,474],[100,375],[300,200],[520,200],[800,375],[800,475]]) # for driving segment 
+area_to_remove=np.array([[250,650],[450,500],[850,500],[1050,650]]) # for youtube video
+
 
 def draw_lanes(image,lanes):
     try:
@@ -68,13 +70,14 @@ def findLanes(videoName):
         lines,lanes,gradients=detect_lines(roi_edges)
         cv2.imshow('lines edges',lines)
         for i in range(len(gradients)):
-            if gradients[i]>0: #LINE 2
+            if gradients[i]>0.5: #LINE 2
                 #print("right line found with coordinates",lanes[i][0],"and gradients",gradients[i])
                 line2=lanes[i][0]
-            elif gradients[i]<0: #line 1
+            elif gradients[i]<-0.5: #line 1
                 #print("left line found with coordinates",lanes[i][0],"and gradients",gradients[i])
                 line1=lanes[i][0]
         frame_with_lanes=draw_lanes(frame,[line1,line2])
         cv2.imshow('the lanes',frame_with_lanes)
+        #print("gradients",gradients)
 
-findLanes('sample-videos/lane-center.mp4')
+findLanes('sample-videos/test-from-youtube.mp4')
